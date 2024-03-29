c5c1f29a80dc067a1efa28730e3bffeb731764d8
FathGzl <fatihguzel3455@gmail.com>
2024-03-29 12:37:22 +0300

    mongo db configuretion işlemi

--- src/Services/Products/ESourcing.Products/Program.cs
+++ src/Services/Products/ESourcing.Products/Program.cs
@@ -1,11 +1,26 @@
+using ESourcing.Products.Settings;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+#region Configuration Dependencies
+builder.Services.Configure<ProductDatabaseSettings>(builder.Configuration.GetSection(nameof(ProductDatabaseSettings)));
+builder.Services.AddSingleton<IProductDatabaseSettings>(sp => sp.GetRequiredService<IOptions<ProductDatabaseSettings>>().Value);
+#endregion
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "ESourcing.Product",
+        Version = "v1"
+    });
+});
 
 var app = builder.Build();
 
