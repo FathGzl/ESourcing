f02737c30f1e9587a4efceac702ed7ffbede968e
FathGzl <fatihguzel3455@gmail.com>
2024-03-29 17:23:44 +0300

    sourcing context nesnesini dependency inject işlemi

--- src/Services/Sourcing/ESourcing.Sourcing/Program.cs
+++ src/Services/Sourcing/ESourcing.Sourcing/Program.cs
@@ -1,12 +1,21 @@
+using ESourcing.Sourcing.Data;
+using ESourcing.Sourcing.Data.Interface;
 using ESourcing.Sourcing.Settings;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+#region Configuration Dependencies
 builder.Services.Configure<ISourcingDatabaseSettings>(builder.Configuration.GetSection(nameof(SourcingDatabaseSettings)));
 builder.Services.AddSingleton<ISourcingDatabaseSettings>(sp => sp.GetRequiredService<IOptions<SourcingDatabaseSettings>>().Value);
+#endregion
+
+#region Prodject Dependencies
+builder.Services.AddTransient<ISourcingContext, SourcingContext>();
+#endregion
+
+
 
 builder.Services.AddControllers();
 
