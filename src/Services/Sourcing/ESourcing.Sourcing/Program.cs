8f9783a1653506e65258b7ab0f0c9dcbc9aeef2b
FathGzl <fatihguzel3455@gmail.com>
2024-03-29 17:14:53 +0300

    sourcing mongodb configuration işlemi

--- src/Services/Sourcing/ESourcing.Sourcing/Program.cs
+++ src/Services/Sourcing/ESourcing.Sourcing/Program.cs
@@ -1,6 +1,12 @@
+using ESourcing.Sourcing.Settings;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.Configure<ISourcingDatabaseSettings>(builder.Configuration.GetSection(nameof(SourcingDatabaseSettings)));
+builder.Services.AddSingleton<ISourcingDatabaseSettings>(sp => sp.GetRequiredService<IOptions<SourcingDatabaseSettings>>().Value);
 
 builder.Services.AddControllers();
 
