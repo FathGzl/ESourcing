0c3d69a9c080c3bd9b86843d5e0a0e4fefb83243
FathGzl <fatihguzel3455@gmail.com>
2024-03-29 18:19:52 +0300

    ...

--- src/Services/Sourcing/ESourcing.Sourcing/Entities/Auction.cs
+++ src/Services/Sourcing/ESourcing.Sourcing/Entities/Auction.cs
@@ -13,9 +13,9 @@ namespace ESourcing.Sourcing.Entities
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ProductId { get; set; }
         public int Quantity { get; set; }
         public DateTime StartedAt { get; set; }
         public DateTime FinishedAt { get; set; }
--- src/Services/Sourcing/ESourcing.Sourcing/Entities/Bid.cs
+++ src/Services/Sourcing/ESourcing.Sourcing/Entities/Bid.cs
@@ -8,9 +8,9 @@ namespace ESourcing.Sourcing.Entities
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        public string AuctionId { get; set; }
-        public string ProductId { get; set; }
-        public string SellerUserName { get; set; }
+        public string? AuctionId { get; set; }
+        public string? ProductId { get; set; }
+        public string? SellerUserName { get; set; }
         public decimal Price { get; set; }
         public DateTime CreatedAt { get; set; }
     }
