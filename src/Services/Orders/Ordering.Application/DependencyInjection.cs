c590df11fefda65e2bab0fdf88555d1c305acfbb
FathGzl <fatihguzel3455@gmail.com>
2024-04-04 11:45:59 +0300

    order create handler ve automapper profil

--- src/Services/Orders/Ordering.Application/DependencyInjection.cs
+++ src/Services/Orders/Ordering.Application/DependencyInjection.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using AutoMapper;
+using Ordering.Application.Mapper;
 
 namespace Ordering.Application
 {
@@ -13,6 +14,17 @@ namespace Ordering.Application
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
+
+
+            #region Configure Mapper
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cfg.AddProfile<OrderMappingProfile>();
+            });
+            var mapper = config.CreateMapper();
+            #endregion
+
 
             return services;
         }
