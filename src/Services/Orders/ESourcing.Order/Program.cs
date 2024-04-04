bb31e00d8d5be348fab595bf07a3d0eae3ab8ec2
FathGzl <fatihguzel3455@gmail.com>
2024-04-04 16:12:22 +0300

    EvenBus Library ile Order Microservice Üzerinde Order Consumer Geliştirmesi

--- src/Services/Orders/ESourcing.Order/Program.cs
+++ src/Services/Orders/ESourcing.Order/Program.cs
@@ -1,10 +1,50 @@
 using ESourcing.Order.Extensions;
 using Ordering.Infrastructure;
 using Ordering.Application;
+using EventBusRabbitMQ.Producer;
+using EventBusRabbitMQ;
+ 
+using Microsoft.AspNetCore.Connections;
+using RabbitMQ.Client;
+using ESourcing.Order.Consumers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+#region EventBus
+
+builder.Services.AddSingleton<IRabbitMQPersistentConnection>(sp =>
+{
+    var logger = sp.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
+
+    var factory = new ConnectionFactory()
+    {
+        HostName = builder.Configuration["EventBus:HostName"]
+    };
+
+    if (!string.IsNullOrWhiteSpace(builder.Configuration["EventBus:UserName"]))
+    {
+        factory.UserName = builder.Configuration["EventBus:UserName"];
+    }
+
+    if (!string.IsNullOrWhiteSpace(builder.Configuration["EventBus:Password"]))
+    {
+        factory.UserName = builder.Configuration["EventBus:Password"];
+    }
+
+    var retryCount = 5;
+    if (!string.IsNullOrWhiteSpace(builder.Configuration["EventBus:RetryCount"]))
+    {
+        retryCount = int.Parse(builder.Configuration["EventBus:RetryCount"]);
+    }
+
+    return new DefaultRabbitMQPersistentConnection(factory, retryCount, logger);
+});
+builder.Services.AddSingleton<EventBusOrderCreateConsumer>();
+#endregion
+
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c => {
@@ -33,6 +73,7 @@ if (app.Environment.IsDevelopment())
     });
 }
 
+app.UseRabbitListener();
 app.UseAuthorization();
 
 app.MapControllers();
