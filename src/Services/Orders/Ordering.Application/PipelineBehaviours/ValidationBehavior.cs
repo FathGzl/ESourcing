2bedaf23dd8b18d57c01c2a5e3ff440786600df1
FathGzl <fatihguzel3455@gmail.com>
2024-04-04 12:33:27 +0300

    ValidationBehavior Pipline işlemi

---
+++ src/Services/Orders/Ordering.Application/PipelineBehaviours/ValidationBehavior.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.PipelineBehaviours
+{
+    public class ValidationBehavior<TRequset, TResponse> : IPipelineBehavior<TRequset, TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequset>> _validators;
+        public Task<TResponse> Handle(TRequset request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TRequset>(request);
+            var failures = _validators.Select(x => x.Validate(context))
+                                       .SelectMany(x => x.Errors)
+                                       .Where(x => x != null)
+                                       .ToList();
+
+            if (failures.Any())
+            {
+                throw new ValidationException(failures);
+            }
+            return next();
+        }
+    }
+}
