184496f94d879bf029ccfb0f0bf33c7f0bab393c
FathGzl <fatihguzel3455@gmail.com>
2024-04-04 11:57:39 +0300

    swagger işlemi

--- src/Services/Orders/ESourcing.Order/Program.cs
+++ src/Services/Orders/ESourcing.Order/Program.cs
@@ -7,12 +7,31 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "ESourcing.Products",
+        Version = "v1"
+    });
+});
+
+
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 //builder.Services.AddInfrastructure();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sourcing API V1");
+    });
+}
 
 app.UseAuthorization();
 
