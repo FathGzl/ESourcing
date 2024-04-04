a387f9445f7f95d5a2b5c926d7472cf0b5c262fa
FathGzl <fatihguzel3455@gmail.com>
2024-04-04 11:34:47 +0300

    create commad ve validator

---
+++ src/Services/Orders/Ordering.Application/Responses/OrderResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Application.Responses
+{
+    public class OrderResponse
+    {
+        public string Id { get; set; }
+        public string AuctionId { get; set; }
+        public string SellerUserName { get; set; }
+        public string ProductId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
