b2043d0827a576fbdd6aab731afac0997c80ac42
FathGzl <fatihguzel3455@gmail.com>
2024-04-04 10:56:55 +0300

    Order ve Base Repository ve Dependenct InMemory işlemi

--- src/Services/Orders/ESourcing.Order/Program.cs
+++ src/Services/Orders/ESourcing.Order/Program.cs
@@ -1,11 +1,14 @@
 using ESourcing.Order.Extensions;
+using Ordering.Infrastructure;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddInfrastructure(builder.Configuration);
+//builder.Services.AddInfrastructure();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ src/Services/Orders/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Domain.Repositories.Base;
+using Ordering.Domain.Repositories;
+using Ordering.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Infrastructure.Repositories.Base;
+using Ordering.Infrastructure.Repositories;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(opt => opt.UseInMemoryDatabase(databaseName: "InMemoryDb"),
+                                                ServiceLifetime.Singleton,
+                                                ServiceLifetime.Singleton);
+
+
+            //Add Repositories
+
+            return services;
+        }
+    }
+}
